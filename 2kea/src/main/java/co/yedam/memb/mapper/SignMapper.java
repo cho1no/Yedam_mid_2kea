929c3cba772c6318b58caa42d2c617aeffafa7a0
gimhyeonjung <gimhyeonjung@localhost>
2024-04-22 17:44:44 +0900

    khj

--- 2kea/src/main/java/co/yedam/memb/mapper/SignMapper.java
+++ 2kea/src/main/java/co/yedam/memb/mapper/SignMapper.java
@@ -1,6 +1,10 @@
 package co.yedam.memb.mapper;
 
+import co.yedam.memb.vo.MemberVO;
+
 public interface SignMapper {
+	public MemberVO selectMember(MemberVO vo);
 	public boolean insertMember();
-	public int deleteMember();
+	public int deleteMember(int id);
+	
 }
