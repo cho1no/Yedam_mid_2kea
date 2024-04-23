6180d8a1b73c7d4d986595379628ea1fd6e2f092
admin <admin@YD05-01>
2024-04-23 17:57:05 +0900

    KJM_DelAsk, EditAsk 추가, AskMapper 수정

--- 2kea/src/main/java/co/yedam/asks/control/DelAsk.java
+++ 2kea/src/main/java/co/yedam/asks/control/DelAsk.java
@@ -6,13 +6,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import co.yedam.asks.service.AskService;
+import co.yedam.asks.service.AskServiceImpl;
 import co.yedam.common.Control;
 
 public class DelAsk implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String askNo = req.getParameter("askNo");
+
+		AskService svc = new AskServiceImpl();
+		if (svc.delAsk(Integer.parseInt(askNo))) {
+			resp.getWriter().print("{\"retCode\": \"Success\"}");
+		} else {
+			resp.getWriter().print("{\"retCode\": \"Fail\"}");
+		}
 
 	}
 
