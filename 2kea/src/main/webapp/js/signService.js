535bbf3080438ac91c4c4c127b07e25e21dd31b0
gimhyeonjung <gimhyeonjung@localhost>
2024-04-30 15:03:32 +0900

    khj

--- 2kea/src/main/webapp/js/signService.js
+++ 2kea/src/main/webapp/js/signService.js
@@ -2,44 +2,65 @@
  * 
  */
 
+/*
 function signIn(successCall, errorCall) {
 	fetch('signInControl.do', {
 		method: 'post',
 		headers: { 'Content-type': 'application/x-www-form-urlencoded' },
 		body: 'id=' + vo.id + '&pw=' + vo.pw
 	})
-		.then(resolve => resolve.json())
+		.then(response => response.json())
 		.then(successCall)
 		.catch(errorCall)
+}*/
+
+const sign_svc = {
+	signUp() {
+		fetch('signUpControl.do', {
+			method: 'post',
+			headers: { 'Content-type': 'application/x-www-form-urlencoded' },
+			body: 'id=' + id + '&pw=' + pw + '&mName=' + mName + '&email='
+				+ email + '&phone=' + phone
+		})
+			.then(response => response.json())
+			.then(succesCall)
+			.error(errorCall)
+	}
 }
 
 function signUp() {
-
 	var id = document.querySelector('input[name="id"]').value;
 	var pw = document.querySelector('input[name="pw"]').value;
 	var mName = document.querySelector('input[name="mName"]').value;
 	var email = document.querySelector('input[name="email"]').value;
 	var phone = document.querySelector('input[name="phone"]').value;
 
-	fetch('signUpControl.do', {
-		method: 'post',
-		headers: { 'Content-type': 'application/x-www-form-urlencoded' },
-		body: 'id=' + id + '&pw=' + pw + '&mName=' + mName + '&email='
-			+ email + '&phone=' + phone
-	})
-		.then(response => response.json())
-		.then(data => {
+	sign_svc.signInfo(id, pw, mName, email, phone)
+		, function(data) {
 			if (data) {
 				alert("회원가입이 되었습니다.")
 				window.location.href = 'prodMain.do';
 			}
-		})
-		.catch(error => {
+		},
+		function(error) {
 			if (error) {
 				alert("회원가입에 실패했습니다.")
 			}
+		}
+}
 
-		})
+function signUp1() {
+
+
+
+
+	then(data => {
+		if (data) {
+			alert("회원가입이 되었습니다.")
+			window.location.href = 'prodMain.do';
+		}
+	})
+		.catch()
 
 }
 
@@ -68,11 +89,6 @@ function findId() {
 			console.error('Error occurred:', error);
 		});
 }
-
-
-
-
-
 
 function findPw() {
 	var mName = document.querySelector('input[id="mName"]').value;
